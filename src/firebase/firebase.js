74c172c700de2b038a5708ac38d8f589fef1e8dd
Ankit Sharma <ankitsh474@gmail.com>
2024-05-11 21:51:29 +0530

    addded

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection} from 'firebase/firestore'
+import dotenv from "dotenv"
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APP_API_KEY,
+  authDomain: "filmyverse-ff6e3.firebaseapp.com",
+  projectId: "filmyverse-ff6e3",
+  storageBucket: "filmyverse-ff6e3.appspot.com",
+  messagingSenderId: "72410487791",
+  appId: "1:72410487791:web:78db415c67ca285589f41a",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const moviesRef = collection(db, "movies");
+export const reviewsRef = collection(db, "reviews");
+export const usersRef = collection(db, "users");
+
+export default app;
